e42639cc3473f18f9ff120a5f826a541f507876e
pavan <pavankumarbugatha036@gmail.com>
2024-04-26 16:07:53 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Todos from './todo';
-import axios from 'axios';
-import Propscomp from './passingprops';
-import { useState } from 'react';
-import Counter from './counter';
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './components/ReactRouter/Home';
 import Reactroutes from './components/ReactRouter/routes';
 import NestedRoute from './components/ReactRouter/home1';
 import SideNav from './components/sidenav/sidenav';
 import LandingComponent from './components/LandingPage/landingPage';
-import { useEffect} from 'react';
 
 
 
 function App() {
 
-  
+
  
   
   return (
